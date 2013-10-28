fd699a7b075c64e125196a7e499e480dc506dbe6
Trevor Gerhardt <trevorgerhardt@gmail.com>
2013-10-28 14:12:48 -0400

    Commented out drag and drop, full working example now

--- example/index.js
+++ example/index.js
@@ -3,16 +3,19 @@
 
 var d3 = require('d3');
 var select = require('select');
+var Set = require('set');
 var Transitive = require('transitive');
-
-// create transitive
-
-var transitive = new Transitive(document.getElementById('canvas'), DEFAULT_DATA);
 
 // create and handle route/pattern selection
 
 var $canvas = document.getElementById('canvas');
 var $form = document.getElementById('form');
+
+// create transitive
+
+new Transitive($canvas, DEFAULT_DATA);
+
+// handle selects
 
 var Routes = select().label('Routes');
 var Patterns = select().multiple().label('Patterns');
@@ -22,16 +25,24 @@ document.getElementById('select-pattern').appendChild(Patterns.el);
 
 Routes.on('select', function (option) {
   Patterns.empty();
-  var route = getRoute(INDEX.routes, option.value);
+  var route = getRoute(INDEX_FULL.routes, option.value) || getRoute(INDEX_FULL.routes, option.name);
   for (var i in route.patterns) {
     var pattern = route.patterns[i];
     Patterns.add(pattern.pattern_name, pattern.pattern_id);
     Patterns.select(pattern.pattern_name.toLowerCase());
   }
 
-  // load the route data & render
-  transitive.load(route).render();
+  /* load the route data & render */
+  $canvas.innerHTML = '';
+  new Transitive($canvas, {
+    routes: [ route ],
+    stops: getStops(INDEX_FULL.stops, route)
+  });
 });
+
+Patterns.on('change', function (patterns) {
+  console.log(patterns.values());
+})
 
 for (var i in INDEX.routes) {
   var route = INDEX.routes[i];
@@ -45,5 +56,19 @@ function getRoute(routes, id) {
       return route;
     }
   }
+}
+
+function getStops(stops, route) {
+  var stop_ids = new Set();
+  for (var i in route.patterns) {
+    var pattern = route.patterns[i];
+    for (var j in pattern.stops) {
+      stop_ids.add(pattern.stops[j].stop_id);
+    }
+  }
+
+  return stops.filter(function (stop) {
+    return stop_ids.has(stop.stop_id);
+  })
 }
 
