21127660a832af8f91b45158915f78899845a3e2
Trevor Gerhardt <trevorgerhardt@gmail.com>
2013-12-09 19:58:10 -0500

    Included data in lib

--- styler/lib/boot/index.js
+++ styler/lib/boot/index.js
@@ -3,98 +3,95 @@
  */
 
 var CodeMirror = require('codemirror');
+var Index = require('./wmata');
 var each = require('each');
 var select = require('select');
 var Sett = require('set');
 var Transitive = require('transitive');
 
-/**
- * Load the data
- */
+//
 
-get_data(function(Index) {
-  var DIRECTION = '0';
-  var ROUTE = null;
-  var STOPS = new Sett();
+var DIRECTION = '0';
+var ROUTE = null;
+var STOPS = new Sett();
 
-  // handle selects
-  var Routes = select()
-    .label('Routes');
-  var Patterns = select()
-    .multiple()
-    .label('Patterns');
+// handle selects
+var Routes = select()
+  .label('Routes');
+var Patterns = select()
+  .multiple()
+  .label('Patterns');
 
-  document.getElementById('select-route')
-    .appendChild(Routes.el);
-  document.getElementById('select-pattern')
-    .appendChild(Patterns.el);
+document.getElementById('select-route')
+  .appendChild(Routes.el);
+document.getElementById('select-pattern')
+  .appendChild(Patterns.el);
 
-  // transitive instance
-  var transitive = new Transitive(document.getElementById('canvas'), Index);
+// transitive instance
+var transitive = new Transitive(document.getElementById('canvas'), Index);
 
-  // Set up filters
-  transitive
-    .filter('stops', function(stop) {
-      return STOPS.has(stop.stop_id);
-    })
-    .filter('routes', function(route) {
-      return ROUTE.route_id === route.route_id;
-    })
-    .filter('patterns', function(pattern) {
-      return Patterns.values()
-        .indexOf(pattern.pattern_id) !== -1;
-    });
-
-  // Direction check box
-  var $reverse = document.getElementById('reverse-direction');
-
-  // on direction change
-  $reverse.addEventListener('change', function(event) {
-    DIRECTION = event.target.checked ? '1' : '0';
-
-    // only show appropriate patterns
-    updatePatterns(transitive, Patterns, ROUTE, DIRECTION);
-    transitive.render();
+// Set up filters
+transitive
+  .filter('stops', function(stop) {
+    return STOPS.has(stop.stop_id);
+  })
+  .filter('routes', function(route) {
+    return ROUTE.route_id === route.route_id;
+  })
+  .filter('patterns', function(pattern) {
+    return Patterns.values()
+      .indexOf(pattern.pattern_id) !== -1;
   });
 
-  // On route selection change
-  Routes.on('select', function(option) {
-    localStorage.setItem('selected-route', option.name);
+// Direction check box
+var $reverse = document.getElementById('reverse-direction');
 
-    ROUTE = getRoute(option.name, Index.routes);
-    STOPS = getStopIds(ROUTE);
+// on direction change
+$reverse.addEventListener('change', function(event) {
+  DIRECTION = event.target.checked ? '1' : '0';
 
-    // only show appropriate patterns
-    updatePatterns(transitive, Patterns, ROUTE, DIRECTION);
-    transitive.render();
-  });
+  // only show appropriate patterns
+  updatePatterns(transitive, Patterns, ROUTE, DIRECTION);
+  transitive.render();
+});
 
-  // add routes
-  for (var i in Index.routes) {
-    var route = Index.routes[i];
-    Routes.add(route.route_id);
-  }
+// On route selection change
+Routes.on('select', function(option) {
+  localStorage.setItem('selected-route', option.name);
 
-  // Select the first route
-  Routes.select(localStorage.getItem('selected-route') || Index.routes[0].route_id
-    .toLowerCase());
+  ROUTE = getRoute(option.name, Index.routes);
+  STOPS = getStopIds(ROUTE);
 
-  $('form').on('submit', function (event) {
-    event.preventDefault();
-    var color = $('input[name="pattern-stroke"]').val();
-    var width = $('input[name="pattern-stroke-width"]').val();
-    transitive.style.load({
-      'patterns': {
-        'stroke-width': function (display) {
-          return width;
-        },
-        'stroke': function (display) {
-          return color;
-        }
+  // only show appropriate patterns
+  updatePatterns(transitive, Patterns, ROUTE, DIRECTION);
+  transitive.render();
+});
+
+// add routes
+for (var i in Index.routes) {
+  var route = Index.routes[i];
+  Routes.add(route.route_id);
+}
+
+// Select the first route
+Routes.select(localStorage.getItem('selected-route') || Index.routes[0].route_id
+  .toLowerCase());
+
+$('form').on('submit', function (event) {
+  event.preventDefault();
+  var color = $('input[name="pattern-stroke"]').val();
+  var width = $('input[name="pattern-stroke-width"]').val();
+  transitive.style.load({
+    'patterns': {
+      'stroke-width': function (display) {
+        return width;
+      },
+      'stroke': function (display) {
+        return color;
       }
-    });
-    transitive.render();
+    }
   });
+  transitive.render();
 });
 
 /**
@@ -142,11 +139,4 @@ function getStopIds(route) {
   }
 
   return stop_ids;
-}
-
-function get_data(cb) {
-  Transitive.d3.json('/styler/data.json', function(err, data) {
-    if (err) return window.alert(err);
-    else cb(data);
-  });
 }
