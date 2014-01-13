f1f6cc49d99d593ee65abc7a75c6569ace9556c4
David Emory <demory@conveyal.com>
2014-01-13 15:30:16 -0500

    render from/to access segments when data is provided

--- test2d/index.js
+++ test2d/index.js
@@ -6,7 +6,22 @@ var transitive = new Transitive(document.getElementById('canvas'));
 
 var endpoint = 'http://arlington.dev.conveyal.com/otp/otp-rest-servlet/';
 
+var config = {
+  maxOptions: 1,
+  fromLocation: {
+    name: 'from',
+    lat: 38.895,
+    lon: -77.09
+  },
+  toLocation: {
+    name: 'to',
+    lat: 38.894,
+    lon: -77.01
+  }
+};
+
+
 transitive.loadProfile(PROFILE, endpoint, function(data) {
   console.log('loaded profiler data');
   transitive.render();
-}, 1);
+}, config);
